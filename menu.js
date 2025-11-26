cc5483691654f7890850050cfa97e0a1044fe982
gtkrprivateserver <gtkrprivateserver@gmail.com>
2025-11-27 00:39:14 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuItems = [
     { name: "Home", link: "/index.html" },
     { name: "About", link: "/about.html" },
+    { name: "Role Exclusive", link: "/exclusive/role.html" },
     { name: "Content Creator", link: "/content/creator.html" },
     { name: "Video", link: "/video/play.html" },
     { name: "Contact Us", link: "https://onedevofficial.vercel.app/contact.html"}
