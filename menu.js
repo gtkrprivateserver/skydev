36f2132011c5b39eb54ef9156a1fc480805908fb
gtkrprivateserver <gtkrprivateserver@gmail.com>
2025-11-27 08:12:08 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuItems = [
     { name: "Home", link: "/index.html" },
     { name: "About", link: "/about.html" },
-    { name: "GTKRPS How To Play", link: "/gtkrps/how-to-play.html" },
+    { name: "GTKRPS", link: "/gtkrps/gtkrps.html" },
     { name: "Role Exclusive", link: "/exclusive/role.html" },
     { name: "Content Creator", link: "/content/creator.html" },
     { name: "Video", link: "/video/play.html" },
